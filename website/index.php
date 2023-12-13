79acaa9f91ed95df4152a412728ab2667f8fc36d
Leonardo Tassinari <leonardo.tassinari6@studio.unibo.it>
2023-12-13 11:42:06 +0100

    test for server

--- website/index.php
+++ website/index.php
@@ -1,10 +1,10 @@
 <?php
-require_once("bootstrap.php");
+/*require_once("bootstrap.php");
 
 $templateparams["title"] = "Hilfe - Home";
 $templateparams["name"] = "explore.php";
 $templateparams["articles"] = $dbh->getHelpPosts(5);
-
-require("template/explore.php");
+*/
+require("html/home.html");
 
 ?>
