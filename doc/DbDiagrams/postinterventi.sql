10e6584f74523fb8b7c0e8114aba3c91ada742ec
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2023-12-23 12:35:17 +0100

    update post interventi file

--- doc/DbDiagrams/postinterventi.sql
+++ doc/DbDiagrams/postinterventi.sql
@@ -1,66 +1,20 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 06, 2023 at 06:07 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Inserimento utenti
+INSERT INTO `HilfeDb`.`User` (`idUser`, `Name`, `Surname`, `PhoneNumber`, `Email`, `Salt`, `Password`, `Bio`, `Birthday`, `PubKey`, `FotoProfilo`, `Username`)
+VALUES
+(1, 'Mario', 'Rossi', '123456789', 'mario@email.com', 'salt123', 'password123', 'Bio di Mario', '1990-01-01', 'pubkey123', null, 'mario_username'),
+(2, 'Luigi', 'Verdi', '987654321', 'luigi@email.com', 'salt456', 'password456', 'Bio di Luigi', '1995-05-05', 'pubkey456', null, 'luigi_username'),
+(3, 'Francesca', 'Bianchi', '555555555', 'francesca@email.com', 'salt789', 'password789', 'Bio di Francesca', '1988-10-15', 'pubkey789', null, 'francesca_username'),
+(4, 'Alessandro', 'Neri', '111111111', 'alessandro@email.com', 'salt111', 'password111', 'Bio di Alessandro', '1985-07-20', 'pubkey111', null, 'alessandro_username'),
+(5, 'Giulia', 'Ricci', '222222222', 'giulia@email.com', 'salt222', 'password222', 'Bio di Giulia', '1992-03-12', 'pubkey222', null, 'giulia_username'),
+(6, 'Stefano', 'Moretti', '333333333', 'stefano@email.com', 'salt333', 'password333', 'Bio di Stefano', '1987-08-27', 'pubkey333', null, 'stefano_username');
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `hilfedb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `postinterventi`
---
-
-CREATE TABLE `postinterventi` (
-  `idPostIntervento` int(11) NOT NULL,
-  `TitoloPost` varchar(100) DEFAULT NULL,
-  `DescrizionePost` varchar(500) DEFAULT NULL,
-  `DataIntervento` datetime DEFAULT NULL,
-  `DataPubblicazione` datetime DEFAULT NULL,
-  `PersoreRichieste` int(10) UNSIGNED DEFAULT NULL,
-  `PosizioneLongitudine` decimal(6,0) DEFAULT NULL,
-  `PosizioneLatitudine` decimal(6,0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Dumping data for table `postinterventi`
---
-
-INSERT INTO `postinterventi` (`idPostIntervento`, `TitoloPost`, `DescrizionePost`, `DataIntervento`, `DataPubblicazione`, `PersoreRichieste`, `PosizioneLongitudine`, `PosizioneLatitudine`) VALUES
-(1, 'Sgombro acqua', 'Ciao! Avrei bisogno di qualcuno che venga ad aiutare a togliere l\'acqua accumulata nel garage di casa mia.\r\n', '2023-12-07 17:51:18', '2023-12-05 17:51:18', 20, 42, 12),
-(2, 'Sgombro casa', 'Buongiorno! C\'è una signora in fondo alla mia via che ha subito molti danni a causa dell\'alluvione e avrebbe bisogno per svuotare casa e pulirla a fondo da tutto il fango. Qualcuno sarebbe disponibile per andare in gruppo ad aiutarla? \r\n', '2023-12-21 18:00:48', '2023-12-01 18:00:48', 2, 42, 15),
-(3, 'Pulizia casa', 'Ciao a tutti, la mia via di casa è completamente ricoperta dal fango, qualcuno è disponibile per venire ad aiutarci a ripulirla?\r\n', '2023-12-09 18:00:48', '2023-12-04 18:00:48', 20, 42, 13),
-(4, 'Richiesta rifocillamento', 'Ciao! Nella mia via siamo ormai tanti ad aiutare a ripulire però se qualcuno si offrisse di portare qualche panino, piadina o un pasto qualsiasi ci farebbe un grande favore. Sarebbero graditissime anche delle bottiglie d\'acqua. Grazie. \r\n\r\n', '2023-12-14 18:02:42', '2023-12-03 18:02:42', 1, 42, 15),
-(6, 'Aiuto materiali', 'Buongiorno! La mia via è stata particolarmente danneggiata e molte persone stanno venendo ad aiutare però i materiali non sono sufficienti. Chiunque avesse qualcuno dei materiali elencati sotto ci farebbe un favore enorme se ce li portasse. Grazie in anticipo. \r\n', '2023-12-22 18:04:34', '2023-12-03 18:04:34', 5, 42, 12),
-(7, 'Pulizia parco', 'Qualcuno oggi è disponibile per venire a ripulire il parco urbano?', '2023-12-08 18:04:34', '2023-12-04 18:04:34', 20, 42, NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `postinterventi`
---
-ALTER TABLE `postinterventi`
-  ADD PRIMARY KEY (`idPostIntervento`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Inserimento post interventi
+INSERT INTO `HilfeDb`.`PostInterventi` (`idPostIntervento`, `TitoloPost`, `DescrizionePost`, `DataIntervento`, `DataPubblicazione`, `PersoneRichieste`, `PosizioneLongitudine`, `PosizioneLatitudine`, `Indirizzo`, `Autore_idUser`)
+VALUES
+(1, 'Emergenza Alluvione in Via Roma', 'Richiedo l\'aiuto della comunità per affrontare una situazione di alluvione in corso. Abbiamo bisogno di volontari per spostare oggetti, distribuire aiuti e assistere le persone evacuate. L\'intervento è pianificato per domani mattina alle 9:00.', '2023-12-24 09:00:00', '2023-12-23 15:30:00', 10, 41.9027835, 12.4963655, 'Via Roma, 123', 1),
+(2, 'Aiuto per anziano bisognoso', 'Sto cercando persone disponibili ad aiutare un anziano residente nella zona. Il compito includerà fare la spesa, pulire la casa e fornire compagnia. Chiunque possa dedicare del tempo, per favore, si faccia avanti.', '2023-12-25 10:30:00', '2023-12-23 16:45:00', 2, 41.8902102, 12.4922311, 'Via Verdi, 456', 2),
+(3, 'Supporto dopo il terremoto', 'Dopo l\'ultimo terremoto, molte famiglie hanno bisogno di aiuto. Cerchiamo volontari per distribuire cibo, coperte e fornire sostegno emotivo. L\'intervento è fissato per il prossimo fine settimana.', '2023-12-30 11:00:00', '2023-12-23 17:30:00', 15, 41.9100711, 12.5359979, 'Via Terremoto, 789', 3),
+(4, 'Assistenza per malato in isolamento', 'Cerco volontari disposti ad aiutare un malato in isolamento a causa di una malattia contagiosa. Le attività includono la consegna di cibo, farmaci e fornire supporto morale. Grazie in anticipo per il vostro aiuto!', '2023-12-28 14:00:00', '2023-12-23 18:15:00', 5, 41.895464, 12.482324, 'Via Salute, 456', 4),
+(5, 'Sostegno psicologico per incidente stradale', 'Sono uno psicologo che offre sostegno psicologico gratuito alle vittime di incidenti stradali. Se hai bisogno di aiuto o conosci qualcuno che potrebbe beneficiarne, non esitare a contattarmi.', '2023-12-26 15:30:00', '2023-12-23 19:00:00', 3, 41.8992265, 12.4958232, 'Via Sostegno, 789', 5);
