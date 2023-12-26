0a87a22a25f508d97f5538b82de16f70cd2d59c9
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2023-12-26 12:41:28 +0100

    move openPopup script

--- website/js/buttonHandler.js
+++ website/js/buttonHandler.js
@@ -75,3 +75,13 @@ function togglePartecipa(idPost, maxPeople) {
         }
     });
 }
+
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
--- website/template/home.php
+++ website/template/home.php
@@ -71,17 +71,6 @@
                     <button class="closePopup" onclick="closePopup()">Indietro</button>
                 </div>
                 <div id="overlay"></div>
-                <script>
-                    function openPopup() {
-                        document.getElementById("popup").style.display = "block";
-                        document.getElementById("overlay").style.display = "block";
-                    }
-
-                    function closePopup() {
-                        document.getElementById("popup").style.display = "none";
-                        document.getElementById("overlay").style.display = "none";
-                    }
-                </script>
             </footer>
         </article>
         <?php endforeach; ?>
