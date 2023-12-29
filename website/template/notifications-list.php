50c4d12beec69f2268f254490a1d70224c5136b9
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2023-12-29 12:02:33 +0100

    add date to notifications

--- website/template/notifications-list.php
+++ website/template/notifications-list.php
@@ -5,7 +5,7 @@
         <ul class="elencoNotifiche">
             <?php foreach ($dbh->getNotifications() as $notification) : ?>
                 <li class="notifica <?php if($notification["Letta"]==0) {echo "\" \"daLeggere";}?>"><img src="../<?php echo $dbh->getProfilePic($notification["idUserGeneratore"]); ?>" alt="Profilo <?php echo $notification["idUserGeneratore"];?>">
-                    <div class="infoNotifica"><span><?php $userCreator = $dbh->getUserFromId($notification["idUserGeneratore"]); echo $userCreator["Name"]." ".$userCreator["Surname"]?></span>
+                    <div class="infoNotifica"><span><?php $userCreator = $dbh->getUserFromId($notification["idUserGeneratore"]); echo $userCreator["Name"]." ".$userCreator["Surname"]?></span><p><?php echo $notification["DataCreazione"];?></p>
                         <p class="testo"><?php echo $notification["TestoNotifica"];?></p>
                     </div>
                 </li>
