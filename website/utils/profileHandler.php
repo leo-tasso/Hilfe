ce59eebe2bfd02f5189b0d3425839e3ca1ef3a7a
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2024-01-03 21:55:22 +0100

    add debug data

--- website/utils/profileHandler.php
+++ website/utils/profileHandler.php
@@ -44,6 +44,6 @@ if (isset($_POST['nome'], $_POST['cognome'], $_POST['data'], $_POST['email'], $_
       header('Location: ../profileEdit.php?error="You are not the owner of the profile"');
    }
 } else {
-   echo var_dump($_POST['nome'], $_POST['cognome'], $_POST['data'], $_POST['email'], $_POST['password']);
+   echo var_dump($_POST);
    //header('Location: ../profileEdit.php?error="parameters unset');
 }
